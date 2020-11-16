1ca3112a2cea45fe19f9d5856c3be8d294530b41
jack <1079713849@qq.com>
2020-11-16 18:11:56 +0800

    feat:发布组件库到到npm上面完成

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import TexUI from '../packages'
-
+import TexUI from 'tex-ui'
+import 'tex-ui/dist/tex-ui.css'
 const app = createApp(App)
+app.config.productionTip = false
+
+
 app.use(TexUI)
 app.mount('#app')
